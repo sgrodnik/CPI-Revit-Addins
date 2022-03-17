67fa164fe88fe86bcafa0ca730d3de63c4cf4cb3
SGrodnik <s.grodnik@genpro.ru>
2022-03-17 17:09:27 +0300

    Fix bug of through-numeration without a point

--- CPI Sheets Updater/CPI Sheets Updater/App.cs
+++ CPI Sheets Updater/CPI Sheets Updater/App.cs
@@ -288,11 +288,19 @@ namespace CPI_Sheets_Updater {
                             continue;
                         }
                         if (sheetsBySpecName.ContainsKey(specName)) {
-                            var coSheetsForVS = sheetsBySpecName[specName];
+                            var coSheetsForVS = sheetsBySpecName[specName].OrderBy(s => s.SheetNumber, new NaturalSortComparer<string>()).ToList();
                             var fracPartOfTheLastSheetForVS = GetParts(coSheetsForVS.Last()).fracPart;
                             var fracPartOfTheFirstSheetForVS = GetParts(coSheetsForVS.First()).fracPart;
                             var coSheetsForVSCount = coSheetsForVS.Count();
-                            var CPI_VS_Nomer_lista = Compose_CPI_Nomer_lista(sheet, fracPartOfTheFirstSheetForVS, fracPartOfTheLastSheetForVS);
+                            string CPI_VS_Nomer_lista = "";
+                            if (fracPartOfTheFirstSheetForVS == "" && fracPartOfTheLastSheetForVS == "")
+                            {
+                                CPI_VS_Nomer_lista = Compose_CPI_Nomer_lista(sheet, coSheetsForVS);
+                            }
+                            else
+                            {
+                                CPI_VS_Nomer_lista = Compose_CPI_Nomer_lista(sheet, fracPartOfTheFirstSheetForVS, fracPartOfTheLastSheetForVS);
+                            }
                             sheet.LookupParameter("CPI_ВС Номер листа").Set(coSheetsForVS.Count == 1 ? num : CPI_VS_Nomer_lista);
                             prim = coSheetsForVS.Count == 1 ? "" : $"На {coSheetsForVSCount} лист{GetEnding(coSheetsForVSCount)}";
                             sheet.LookupParameter("CPI_ВС Примечание").Set(prim);
@@ -323,6 +331,95 @@ namespace CPI_Sheets_Updater {
                 }
             }
         }
+        
+        public class NaturalSortComparer<T> : IComparer<string>, IDisposable
+        {
+            private bool isAscending;
+
+            public NaturalSortComparer(bool inAscendingOrder = true)
+            {
+                this.isAscending = inAscendingOrder;
+            }
+
+            #region IComparer<string> Members
+
+            public int Compare(string x, string y)
+            {
+                throw new NotImplementedException();
+            }
+
+            #endregion
+
+            #region IComparer<string> Members
+
+            int IComparer<string>.Compare(string x, string y)
+            {
+                if (x == y)
+                    return 0;
+
+                string[] x1, y1;
+
+                if (!table.TryGetValue(x, out x1))
+                {
+                    x1 = Regex.Split(x.Replace(" ", ""), "([0-9]+)");
+                    table.Add(x, x1);
+                }
+
+                if (!table.TryGetValue(y, out y1))
+                {
+                    y1 = Regex.Split(y.Replace(" ", ""), "([0-9]+)");
+                    table.Add(y, y1);
+                }
+
+                int returnVal;
+
+                for (int i = 0; i < x1.Length && i < y1.Length; i++)
+                {
+                    if (x1[i] != y1[i])
+                    {
+                        returnVal = PartCompare(x1[i], y1[i]);
+                        return isAscending ? returnVal : -returnVal;
+                    }
+                }
+
+                if (y1.Length > x1.Length)
+                {
+                    returnVal = 1;
+                }
+                else if (x1.Length > y1.Length)
+                { 
+                    returnVal = -1; 
+                }
+                else
+                {
+                    returnVal = 0;
+                }
+
+                return isAscending ? returnVal : -returnVal;
+            }
+
+            private static int PartCompare(string left, string right)
+            {
+                int x, y;
+                if (!int.TryParse(left, out x))
+                    return left.CompareTo(right);
+
+                if (!int.TryParse(right, out y))
+                    return left.CompareTo(right);
+
+                return x.CompareTo(y);
+            }
+
+            #endregion
+
+            private Dictionary<string, string[]> table = new Dictionary<string, string[]>();
+
+            public void Dispose()
+            {
+                table.Clear();
+                table = null;
+            }
+        }
 
         private string GetEnding(int count) {
             var ending = (singular: "е", plural: "ах");
@@ -332,6 +429,13 @@ namespace CPI_Sheets_Updater {
         private string Compose_CPI_Nomer_lista(ViewSheet sheet, string fracPartOfTheFirstSheet, string fracPartOfTheLastSheet) {
             (string intPart, string _) = GetParts(sheet);
             return $"{intPart}.{fracPartOfTheFirstSheet} ÷ {intPart}.{fracPartOfTheLastSheet}"; // "12.1 ÷ 12.42"
+        }
+
+        private string Compose_CPI_Nomer_lista(ViewSheet sheet, List<ViewSheet> coSheetsForVs)
+        {
+            var intPartOfTheFirstSheet = GetParts(coSheetsForVs.First()).intPart;
+            var intPartOfTheLastSheet = GetParts(coSheetsForVs.Last()).intPart;
+            return $"{intPartOfTheFirstSheet} ÷ {intPartOfTheLastSheet}";
         }
 
         public (string intPart, string fracPart) GetParts(ViewSheet sheet) {
